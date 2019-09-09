a70e97d5c6d870263273d7382265aba4511c5419
Carrie Schmid and Ethan Samuels-Ellingson <carrie+ethan@>
2019-09-09 16:37:18 -0700

    Add loops for arrays.

--- src/main.js
+++ src/main.js
@@ -8,58 +8,73 @@ import './styles.css';
 
 //frontend
 $(document).ready(function() {
-  let newPet;
+  let pets = [];
   $('form#pet-info').submit(function(event) {
     $('div#no-pet-name').hide();
     event.preventDefault();
     let name = $('input#pet-name').val();
     let type = $('input:radio[name=type]:checked').val();
     if(name){
-      newPet = new Animal(name, type);
-      attachContactListeners(newPet);
+      let newPet = new Animal(name, type);
+
+      pets.push(newPet);
+      attachContactListeners(pets);
+      newPet.setHunger();
+      newPet.setHappiness();
+      setInterval(function() {
+        updateHTML(pets)}, 1000);
     } else {
       $('div#no-pet-name').show();
     }
-    newPet.setHunger();
-    newPet.setHappiness();
-    setInterval(function() {
-      updateHTML(newPet)}, 1000);
-    // animalArray.push(newPet);
   })
-
-
-  $("button#feed").click(function(event){
-    console.log("check");
-    event.preventDefault();
-    newPet.feed();
-  });
 })
 
-function attachContactListeners(newPet) {
+function attachContactListeners(pets) {
   $('div#output').on("click", "button", function() {
-    var id = this.id
-    if(id === "feed"){
-      newPet.feed();
-    }else{
-      newPet.play();
+    let id = this.id;
+    console.log(id);
+    if(id[0] === "f"){
+      let petName = id.slice(4);
+      pets.forEach(function(pet) {
+        if(pet.name === petName) {
+          pet.feed();
+        }
+      });
+    }else if (id[0] === "p") {
+      let petName = id.slice(4);
+      pets.forEach(function(pet) {
+        if(pet.name === petName) {
+          pet.play();
+        }
+      });
+    } else {
+      let petName = id.slice(5);
+      pets.forEach(function(pet) {
+        if(pet.name === petName) {
+          pet.play();
+        }
+      });
     }
   });
 }
 
-function updateHTML(pet) {
+function updateHTML(pets) {
   let newHtml = "";
-  if(pet.isDead === true){
-    newHtml += `<p>${pet.name} is dead.</p>`
-  }else{
-    if(pet.isDepressed === true){
-      newHtml += `<p>${pet.name} is depressed.</p>`;
+  pets.forEach(function(pet){
+
+    if(pet.isDead === true){
+      newHtml += `<p>${pet.name} is dead.</p>`
+    }else{
+      if(pet.isDepressed === true){
+        newHtml += `<p>${pet.name} is depressed.</p>`;
+      }
+      newHtml += `<p>Animal name: ${pet.name}</p>`;
+      newHtml += `<br><p>hunger level: ${pet.foodLevel}</p>`;
+      newHtml += `<br><p>happiness level: ${pet.happinessLevel}`
+      newHtml += `<br><button id= "feed${pet.name}"> Feed </button>`
+      newHtml += `<br><button id= "play${pet.name}"> Play </button>`
+      newHtml += `<br><button id= "sleep${pet.name}"> Sleep </button>`
     }
-    newHtml += `<p>Animal name: ${pet.name}</p>`;
-    newHtml += `<br><p>hunger level: ${pet.foodLevel}</p>`;
-    newHtml += `<br><p>happiness level: ${pet.happinessLevel}`
-    newHtml += `<br><button id= "feed"> Feed </button>`
-    newHtml += `<br><button id= "play"> Play </button>`
-    newHtml += `<br><button id= "sleep"> Sleep </button>`
-  }
+  })
   $('div#output').html(newHtml);
 }
